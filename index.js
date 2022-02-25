c1471f86b90997ac87a66cca326435c35497b1ad
Amaresh Kumar Mali <97349768+amaresh5@users.noreply.github.com>
2022-02-26 02:07:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+
+
+// window.onscroll = () => {
+//     const nav = document.querySelector('#aaaaaa');
+//     if(this.scrollY <= 10) nav.className = ''; else nav.className = 'scroll';
+//   };
+
+
+
